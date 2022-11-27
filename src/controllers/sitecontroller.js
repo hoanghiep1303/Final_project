3f4ce29ff309b26214d94bbab47a89aabc343c5b
hoanghiep1303 <84695032+hoanghiep1303@users.noreply.github.com>
2022-11-27 12:09:46 +0700

    update

--- src/controllers/sitecontroller.js
+++ src/controllers/sitecontroller.js
@@ -72,30 +72,22 @@ class sitecontroller {
                 if (user) {
                     req.flash('error', 'Email is already exist!');
                     res.redirect('/register');
-                    // res.render('register', {
-                    //     failMsg: 'Email is already exist!',
-                    // })
                 } else if (req.body.confirmPassword != req.body.password) {
                     req.flash('error', 'Password does not match!');
                     res.redirect('/register');
-                    // res.render('register', {
-                    //     failMsg: 'Password does not match!',
-                    // })
                 } else {
                     var temp = req.body.password
                     bcrypt.hash(temp, 10, (err, hash) => {
                         const newUser = new User({
                             email: req.body.email,
                             password: hash,
-                            // role: 
+                            name: req.body.name,
+                            avatar: 'sample-avatar.png',
                         })
                         newUser.save(err, result => {
                             if (err) {
                                 req.flash('error', 'Server ERR!');
                                 res.redirect('/register');
-                                // res.render('register', {
-                                //     failMsg: 'Server ERR!',
-                                // })
                             }
                         })
                         return res.redirect('/login')
@@ -111,22 +103,10 @@ class sitecontroller {
                 if (!user) {
                     req.flash('error', 'Email is not exist!');
                     res.redirect('/login');
-                    // res.render('login', {
-                    //     failMsg: 'Email is not exist!'
-                    // })
                 }
                 else {
                     var temp = req.body.password;
                     bcrypt.compare(temp, user.password, (err, result) => {
-                        // if(result){
-                        //     res.render('login', {
-                        //         failMsg: 'Thanh cong'
-                        //     })
-                        // } else {
-                        //     res.render('login', {
-                        //         failMsg: 'Password is incorrect'
-                        //     })
-                        // }
                         if (result) {
                             var token = jwt.sign({ _id: user.id }, 'mytoken');
                             res.cookie('token', token, { maxAge: 2147483647, httpOnly: true });
@@ -134,9 +114,6 @@ class sitecontroller {
                         } else {
                             req.flash('error', 'Password is incorrect');
                             res.redirect('/login');
-                            // return res.render('login', {
-                            //     failMsg: 'Password is incorrect'
-                            // })
                         }
                     })
                 }
@@ -242,7 +219,7 @@ class sitecontroller {
                     "currency": "USD",
                     "total": parseInt(req.body.totalPrice)
                 },
-                "description": "Washing Bar soap"
+                "description": ""
             }]
         };
 
